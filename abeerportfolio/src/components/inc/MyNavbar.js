837528c9c2556d124c8104614677e88ffdfdc895
ql490 <tagrba455@gmail.com>
2024-04-05 03:52:37 +0300

    Project Set Up

---
+++ abeerportfolio/src/components/inc/MyNavbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyNavbar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ abeerportfolio/src/components/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyNavbar from '../inc/MyNavbar'
+
+export default function HomePage() {
+  return (
+    <div>
+      <MyNavbar/>
+    </div>
+  )
+}
